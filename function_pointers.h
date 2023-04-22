980b72387384710a3a0b3b2a761f8ed5914bb53e
toxicfrank <ngugifrancis930@gmail.com>
2023-04-22 06:23:22 +0100

    practice

---
+++ function_pointers.h
@@ -0,0 +1,9 @@
+#include <stddef.h>
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
--- functions_10.c
+++ functions_10.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * 
+ * numb - prototype function
+ * @num: function parameter that stores an integer
+ * Return: void
  */
 /*Using the correct format*/
 void numb(int *num)
@@ -8,6 +10,8 @@ void numb(int *num)
     printf("The number is %d.\n", *num);
 }
 /**
+ * main - calls the function above
+ * Return: always 0
  */
 int main(void)
 {
