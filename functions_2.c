992b47bc14e7a6f3e9b33faee7dbd46ac5b30e0e
toxicfrank <ngugifrancis930@gmail.com>
2023-04-22 03:08:29 +0100

    pratice

---
+++ functions_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * fun - prototype
+ * @a: integer
+ * Return: always 0
+ */
+void fun(int a)
+{
+    printf("Value of a is %d\n", a);
+}
+/**
+ * main - calls the fun(function)
+ * Return: 0
+ */
+int main(void)
+{
+    /*Removing & operator before tje fun function*/
+    void (*fun_ptr)(int) = fun;
+    /*Hence we call the function with the line of code below*/
+    fun_ptr (10);
+
+    return (0);
+}
