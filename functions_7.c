f5b7c373b6288ed113d9f4b274ff62892e427569
toxicfrank <ngugifrancis930@gmail.com>
2023-04-22 04:41:24 +0100

    practice

---
+++ functions_7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_hello - function that prints Hello
+ * Returns: void
+ */
+void print_hello()
+{
+    printf("Hello");
+}
+/**
+ * main - calls the function above
+ * Return: always 0
+ */
+int main(void)
+{
+    void (*ptr)() = &print_hello;
+
+    (*ptr)();
+}
